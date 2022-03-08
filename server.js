5ff1ede0aceaac078f70ca9b33ac06f6d3d3be9a
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-03-07 18:45:48 -0800

    added dotenv for msql login storage

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 // packages needed for this application
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
 // importing questions for inquirer
 const { initialQ, employeeAdd, roleAdd, departmentAdd } = require('./utils/questions');
+
+dotenv.config();
+
+const db = mysql.createConnection(
+    {
+      host: process.env.MYSQL_HOST,
+      // MySQL username,
+      user: process.env.MYSQL_USERNAME,
+      // TODO: Add MySQL password here
+      password: process.env.MYSQL_PW,
+      database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
