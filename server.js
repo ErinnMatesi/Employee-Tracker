f3d3ff7009b51a04f5a5117653e7589c707d259b
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-03-12 15:53:37 -0800

    created most of the functions that will be needed. addDepartment and addEmployee are started. Created overall base function that leads to other functions based on the initial prompt

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // packages needed for this application
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
+const cTable = require('console.table');;
 
 // importing questions for inquirer
 const { initialQ, employeeAdd, roleAdd, departmentAdd } = require('./utils/questions');
@@ -11,11 +12,103 @@ dotenv.config();
 const db = mysql.createConnection(
     {
       host: process.env.MYSQL_HOST,
-      // MySQL username,
       user: process.env.MYSQL_USERNAME,
-      // TODO: Add MySQL password here
       password: process.env.MYSQL_PW,
-      database: 'movies_db'
+      port: process.env.PORT || 3001,
+      database: 'company_db'
     },
-    console.log(`Connected to the movies_db database.`)
-  );
+    console.log(`Connected to the company_db database.`)
+);
+
+db.connect((err) => {
+  if(err) throw err;
+  console.log('connected to db');
+  beginPrompts();
+}
+);
+
+const roles = [];
+function findRole() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roles.push(res[i].title);
+    }
+
+  })
+  return roles;
+}
+
+const managers = [];
+function findManager() {
+  connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managers.push(res[i].first_name);
+    }
+
+  })
+  return managers;
+}
+
+const beginPrompts = () => {
+  inquirer.prompt(initialQ)
+      .then((data) => {
+          if(data.choices === 'View all departments') {
+              viewDepartments();
+          } else if (data.choices === 'View all roles') {
+              viewRoles();
+          } else if (data.choices === 'View all employees') {
+              viewEmployees();
+          } else if (data.choices === 'Add a department') {
+              addDepartment();
+          } else if (data.choices === 'Add a role') {
+              addRole();
+          } else if (data.choices === 'Add an employee') {
+              addEmployee();
+          } else if (data.choices === 'Update an employee role') {
+              updateEmployee();
+          } else {
+              db.end;
+          }
+      })
+}
+
+// VIEW ALL functions
+const viewDepartments = () => {};
+const viewRoles = () => {};
+const viewEmployees = () => {};
+
+// ADD functions
+const addDepartment = () => {
+  inquirer.prompt(departmentAdd)
+  .then((data) => {
+    db.query('INSERT INTO departments (name) VALUES (?)', data.dptName, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(`Added ${data.name} to the departments table.`);
+    })
+  })
+};
+const addRole = () => {};
+const addEmployee = () => {
+  inquirer.prompt(employeeName)
+  .then((data) => {
+    // why the +1?
+    const roleID = findRole().indexOf(data.role) + 1;
+    const managerID = findManager().indexOf(data.manager) + 1;
+    db.query('INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [data.firstName, data.lastName, roleID, managerID], (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(`Added ${data.firstName} ${data.lastName} to the employees table.`);
+      // should I retrigger the beginPrompts function here?
+    })
+  })
+};
+
+// UPDATE Employee Role
+const updateEmployee = () => {};
+
+  // use console table as some point to print the tables ?
--- utils/questions.js
+++ utils/questions.js
@@ -3,7 +3,7 @@ const initialQ = [
         name: 'whatDO',
         type: 'list',
         message: `What would you like to do?`,
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit'],
         default: 'View all departments'
     }
 ];
